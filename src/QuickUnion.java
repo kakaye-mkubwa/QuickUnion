764dd5c5be4598f4f6df9e53646ab02db7868610
kakaye <ian.kyalo@students.jkuat.ac.ke>
2020-01-11 05:55:15 -0500

    Added documentation

--- src/QuickUnion.java
+++ src/QuickUnion.java
@@ -2,6 +2,10 @@ public class QuickUnion {
 
     private int[] id;
 
+    /**
+     * Constructor to initialize each array value as its own root
+     * @param N
+     */
     public QuickUnion(int N) {
         id = new int[N];
         for (int i = 0; i < N; i++) {
@@ -9,15 +13,32 @@ public class QuickUnion {
         }
     }
 
+    /**
+     * Function to find the root of a value in the array
+     * @param i
+     * @return
+     */
     private int root(int i){
         while(i != id[i]) i = id[i];
         return i;
     }
 
+    /**
+     * Function to find if two values are connected i.e have same root
+     * @param p
+     * @param q
+     * @return
+     */
     public boolean isConnected(int p,int q){
         return root(p) == root(q);
     }
 
+    /**
+     * Connect two values in the array
+     * i.e Assign the first value,p, the root of the second,q
+     * @param p
+     * @param q
+     */
     public void union(int p, int q){
         int i = root(p);
         int j = root(q);
